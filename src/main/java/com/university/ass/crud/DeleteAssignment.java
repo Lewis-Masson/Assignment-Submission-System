b2c0005566d7e7acd02237b292ead499a8c4cc53
lewis <lewis@Lewis-PC>
2026-03-13 15:50:47 +0000

    Added delete notifications for student and advisor, fixed mark as read issue.

--- src/main/java/com/university/ass/crud/DeleteAssignment.java
+++ src/main/java/com/university/ass/crud/DeleteAssignment.java
@@ -4,8 +4,11 @@ import com.university.ass.model.AuditLog;
 import com.university.ass.model.User;
 import com.university.ass.repository.AuditLogRepository;
 import com.university.ass.service.AssignmentService;
+import com.university.ass.service.NotificationService;
+import com.university.ass.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
 @Component
 public class DeleteAssignment {
@@ -16,15 +19,45 @@ public class DeleteAssignment {
     @Autowired
     private AuditLogRepository auditLogRepository;
 
-    public void execute(int assignmentId, User deletedBy) {
-        // Audit log before deletion
-        AuditLog log = new AuditLog();
-        log.setUser(deletedBy);
-        log.setAction("DELETE");
-        log.setEntityType("Assignment");
-        log.setEntityId(assignmentId);
-        auditLogRepository.save(log);
+    @Autowired
+    private NotificationService notificationService;
 
-        assignmentService.deleteAssignment(assignmentId);
+    @Autowired
+    private UserService userService;
+
+    public void execute(int assignmentId, User deletedBy) {
+
+        // Fetch assignment before deletion
+        assignmentService.findById(assignmentId).ifPresent(assignment -> {
+
+            // Notify the student
+            notificationService.createNotification(
+                    assignment.getStudent(),
+                    "Your assignment (Ref: " + assignment.getReferenceNumber()
+                    + ") was deleted by your course adviser."
+            );
+
+            // Notify all advisers
+            List<User> advisers = userService.findAllAdvisers();
+            for (User adviser : advisers) {
+                notificationService.createNotification(
+                        adviser,
+                        "You deleted assignment (Ref: " + assignment.getReferenceNumber()
+                        + ") belonging to student " + assignment.getStudent().getFirstName()
+                        + " " + assignment.getStudent().getLastName() + "."
+                );
+            }
+
+            // Audit log before deletion
+            AuditLog log = new AuditLog();
+            log.setUser(deletedBy);
+            log.setAction("DELETE");
+            log.setEntityType("Assignment");
+            log.setEntityId(assignmentId);
+            auditLogRepository.save(log);
+
+            // Now delete
+            assignmentService.deleteAssignment(assignmentId);
+        });
     }
 }
